b69ed99562c060d8f70a04f8560d7c3bf7389f68
atom-tr <thai.tr@hotmail.com>
2024-03-09 19:15:03 +0700

    feat: outline

--- course_outline.js
+++ course_outline.js
@@ -47,3 +47,40 @@ class Course {
 		});
 	}
 }
+
+class CourseOutline extends Course {
+	// <iframe id="course-video-container" src="https://player.vimeo.com/video/{{ $videouri }}?autoplay=true" width="100%" height="auto" style="min-height: 50%" frameborder="0" allow="autoplay"></iframe>
+	
+	render() {
+		const outline = this.get_outline();
+		outline.forEach((section, index, a) => {
+			// render per section
+			let content = this.course_content.cloneNode(true);
+			content.removeAttribute('id');
+			let header = content.querySelector('.course-outline-item-header');
+			header.querySelector('.course-outline-item-order-lerning').innerText = index + 1;
+			header.querySelector('.course-outline-item-title').innerText = section.heading?section.heading:"";
+			// header.querySelector('.course-outline-item-title-wrapper > .text-block').innerText = section.description?section.description:"";
+			let videoList = content.querySelector('.video-item-list');
+			section.lessons.forEach((vi, j, a) => {
+				let video = content.querySelector('.video-item').cloneNode(true);
+				video.querySelector('.video-name').innerText = vi.title?vi.title:"";
+				video.querySelector('.video-length').innerText = vi.duration?vi.duration:"00:00";
+				// video.setAttribute("data-url", vi.url);
+				video.addEventListener("click", (event) => {
+					document.getElementById("course-video-container").setAttribute("src". vi.url);
+					console.log("clicked" + vi.url);
+				});
+				if (j === 0) {
+					videoList.innerHTML = "";
+				} 
+				videoList.append(video)
+			});
+			if (index === 0) {
+				this.outline_container.innerHTML = "";
+			} 
+			this.outline_container.append(content)
+			// return course_content;
+		});
+	}
+}
